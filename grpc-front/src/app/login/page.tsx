27f825c756924152404a8bf1c0e5db0cbafbc7cd
Jumpei Motohashi <61613314+jumpei00@users.noreply.github.com>
2023-08-18 01:02:55 +0900

    ログイン画面の実装

---
+++ grpc-front/src/app/login/page.tsx
@@ -0,0 +1,10 @@
+import Login from "@/components/auth/loginButton";
+
+export default async function Page() {
+    return (
+        <>
+            <h2>ログイン</h2>
+            <Login></Login>
+        </>
+    );
+}
